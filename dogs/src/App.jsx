5773dc06d2e2cfce08694e7c3b3940f5fa7a58ee
sasakikun12 <sasaki121294@gmail.com>
2024-03-19 08:42:54 -0400

    feat: add svg and header/footer components

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login/Login";
 
 function App() {
-
   return (
-    
-      <div>
-        App React
-      </div>
-      
-  
-  )
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
